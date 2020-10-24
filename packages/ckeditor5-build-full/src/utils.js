e0fdb25862e8300926d1a9b6439ba74f32edc8ed
letsbug <biuabiu@qq.com>
2020-10-24 18:18:23 +0800

    types complete...

--- packages/ckeditor5-build-full/src/utils.js
+++
@@ -1,5 +0,0 @@
-import { findOptimalInsertionPosition } from '@ckeditor/ckeditor5-widget/src/utils';
-
-export default {
-  findOptimalInsertionPosition
-}
--- packages/ckeditor5-build-full/types/src/index.d.ts
+++
@@ -1,8 +0,0 @@
-import * as engine from '@ckeditor/ckeditor5-engine';
-import { model } from '../engine';
-
-declare namespace utils {
-  interface Utils {
-	findOptimalInsertionPosition: (selection: engine.model.DocumentSelection, model: model.Model) => engine.model.Position;
-  }
-}
